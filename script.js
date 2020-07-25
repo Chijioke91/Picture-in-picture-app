8f78481609db752a4c6115b75d13a761a9b4bb3a
cjaytechguy <cjaytechguy@gmail.com>
2020-07-25 23:32:19 +0100

    final commit

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('testing');
+const videoElem = document.getElementById('video');
+const buttonElem = document.getElementById('button');
+
+const selectMediaStream = async () => {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElem.srcObject = mediaStream;
+    videoElem.onloadedmetadata = () => videoElem.play();
+  } catch (e) {
+    console.log('Something Went Wrong', e.message);
+  }
+};
+
+buttonElem.addEventListener('click', async () => {
+  buttonElem.disabled = true;
+
+  // start picture in picture preview mode
+  await videoElem.requestPictureInPicture();
+
+  buttonElem.disabled = false;
+});
+
+selectMediaStream();
